92c5cc30ee8b912bf9b87c3ac719a1fc570bed65
Антон <laa2746@gmail.com>
2018-05-09 18:24:29 +0700

    add course_list

--- src/main/java/ru/asmi/servlets/GetCourseListServlet.java
+++ src/main/java/ru/asmi/servlets/GetCourseListServlet.java
@@ -1,7 +1,10 @@
 package ru.asmi.servlets;
 
 import org.apache.log4j.Logger;
+import ru.asmi.pojo.Course;
 import ru.asmi.pojo.Student;
+import ru.asmi.service.CourseService;
+import ru.asmi.service.CourseServiceImpl;
 import ru.asmi.service.StudentService;
 import ru.asmi.service.StudentServiceImpl;
 
@@ -12,19 +15,19 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
-public class GetStudentListServlet extends HttpServlet {
+public class GetCourseListServlet extends HttpServlet {
 
-    private final static Logger logger = Logger.getLogger(GetStudentListServlet.class);
+    private final static Logger logger = Logger.getLogger(GetCourseListServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        StudentService studentService = new StudentServiceImpl();
+        CourseService courseService = new CourseServiceImpl();
         try {
-            List<Student> students = studentService.getStudents();
-            for(Student student : students) resp.getWriter().println("name: " + student.getName() + " soname: " + student.getSoname() + " age: " + student.getAge());
+            List<Course> courses = courseService.getCourses();
+            for(Course course : courses) resp.getWriter().println("course: " + course.getTitle() + " annotation: " + course.getAnnotation());
         } catch (SQLException | NullPointerException e) {
             resp.getWriter().println("Sorry! We have some problems =(");
-            logger.error("Problem with student_list page");
+            logger.error("Problem with course_list page");
             logger.error(e.toString());
         }
     }
